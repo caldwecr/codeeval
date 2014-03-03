5dc388791cc772c1269863b6dab51ce10a964930
Courtland Caldwell <courtland.caldwell@cympel.com>
2014-03-03 15:44:02 -0600

    Initial commit of Heapenson algorithm.

---
+++ HeapensonTests/NormalizerTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 3/3/14
+ * Time: 1:24 PM
+ */ 
---
+++ heapenson.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 3/3/14
+ * Time: 12:47 PM
+ */ 
